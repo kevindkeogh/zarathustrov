69bd3a92fe0a9d6f4e448a142a8dbba589af5a63
Kevin <kevin.d.keogh@gmail.com>
2018-01-01 22:07:46 -0500

    Add secrets file

---
+++ secrets.go
@@ -0,0 +1,15 @@
+package main
+
+const (
+	giphyApiKey string = ""
+
+	redditID       string = ""
+	redditSecret   string = ""
+	redditUsername string = ""
+	redditPassword string = ""
+
+	twitterConsumerKey    string = ""
+	twitterConsumerSecret string = ""
+	twitterAccessToken    string = ""
+	twitterAccessSecret   string = ""
+)
